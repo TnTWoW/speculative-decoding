267ff89cd69d8becbb040f38fcda494e4e058ae4
zyz_home <yuzezhao@mail.ustc.edu.cn>
2023-12-08 08:51:16 +0800

    finish inference.py

--- inference.py
+++ inference.py
@@ -103,3 +103,27 @@ train_dataset = TextSamplerDataset(data_train, SEQ_LEN)
 val_dataset = TextSamplerDataset(data_val, SEQ_LEN)
 train_loader = cycle(DataLoader(train_dataset, batch_size=BATCH_SIZE))
 val_loader = cycle(DataLoader(val_dataset, batch_size=BATCH_SIZE))
+
+approx_model.eval()
+target_model.eval()
+
+inp = random.choice(val_dataset)[:PRIME_LENGTH]
+prime = decode_tokens(inp)
+print(f"%s \n\n %s", (prime, "*" * 100))
+
+prompt = inp[None, ...]
+
+sampled, base_decode_elapsed = benchmark(base_decoding)(target_model, prompt, GENERATE_LENGTH)
+
+(spec_decode_sampled, num_accepted), spec_decode_elapsed = benchmark(speculative_decoding)(approx_model, target_model, prompt,
+                                                                                           GENERATE_LENGTH, GAMMA)
+
+base_decode_output = decode_tokens(sampled[0])
+spec_decode_output = decode_tokens(spec_decode_sampled[0])
+
+print("\nbase decoding:\n\n", base_decode_output, "\n")
+print("\nspec decoding:\n\n", spec_decode_output, "\n")
+
+print(f'base decoding in: {base_decode_elapsed:.3f}ms\n')
+print(f'spec decoding in: {spec_decode_elapsed:.3f}ms\n')
+print(f'average num accepted: {num_accepted:.1f} / {GAMMA}\n')
